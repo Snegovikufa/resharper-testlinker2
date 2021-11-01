95b2448b003fad9669f5e9c0f1005f0170aca7f9
Rustam Safin <rustiksnegovik@gmail.com>
2021-11-01 14:54:23 +0200

    Commit rdgen
    Signed-off-by: Rustam Safin <rustiksnegovik@gmail.com>

--- src/rider/main/kotlin/com/jetbrains/rider/plugins/testlinker2/TestLinker2Model.Generated.kt
+++ src/rider/main/kotlin/com/jetbrains/rider/plugins/testlinker2/TestLinker2Model.Generated.kt
@@ -16,39 +16,167 @@ import kotlin.reflect.KClass
 /**
  * #### Generated from [TestLinker2Model.kt:8]
  */
-class TestLinker2Model internal constructor(
+class TestLinker2Model private constructor(
+    private val _myString: RdOptionalProperty<String>,
+    private val _myBool: RdOptionalProperty<Boolean>,
+    private val _myEnum: RdProperty<MyEnum?>,
+    private val _data: RdMap<String, String>,
+    private val _myStructure: RdSignal<MyStructure>
 ) : RdExtBase() {
     //companion
     
     companion object : ISerializersOwner {
         
         override fun registerSerializersCore(serializers: ISerializers)  {
+            serializers.register(MyEnum.marshaller)
+            serializers.register(MyStructure)
         }
         
         
         
+        private val __MyEnumNullableSerializer = MyEnum.marshaller.nullable()
         
-        const val serializationHash = -6093571232781165861L
+        const val serializationHash = -5776883221166105266L
         
     }
     override val serializersOwner: ISerializersOwner get() = TestLinker2Model
     override val serializationHash: Long get() = TestLinker2Model.serializationHash
     
     //fields
+    val myString: IOptProperty<String> get() = _myString
+    val myBool: IOptProperty<Boolean> get() = _myBool
+    val myEnum: IProperty<MyEnum?> get() = _myEnum
+    val `data`: IMutableViewableMap<String, String> get() = _data
+    val myStructure: ISignal<MyStructure> get() = _myStructure
     //methods
     //initializer
+    init {
+        _myString.optimizeNested = true
+        _myBool.optimizeNested = true
+        _myEnum.optimizeNested = true
+        _data.optimizeNested = true
+    }
+    
+    init {
+        bindableChildren.add("myString" to _myString)
+        bindableChildren.add("myBool" to _myBool)
+        bindableChildren.add("myEnum" to _myEnum)
+        bindableChildren.add("data" to _data)
+        bindableChildren.add("myStructure" to _myStructure)
+    }
+    
     //secondary constructor
+    internal constructor(
+    ) : this(
+        RdOptionalProperty<String>(FrameworkMarshallers.String),
+        RdOptionalProperty<Boolean>(FrameworkMarshallers.Bool),
+        RdProperty<MyEnum?>(null, __MyEnumNullableSerializer),
+        RdMap<String, String>(FrameworkMarshallers.String, FrameworkMarshallers.String),
+        RdSignal<MyStructure>(MyStructure)
+    )
+    
     //equals trait
     //hash code trait
     //pretty print
     override fun print(printer: PrettyPrinter)  {
+        printer.println("TestLinker2Model (")
+        printer.indent {
+            print("myString = "); _myString.print(printer); println()
+            print("myBool = "); _myBool.print(printer); println()
+            print("myEnum = "); _myEnum.print(printer); println()
+            print("data = "); _data.print(printer); println()
+            print("myStructure = "); _myStructure.print(printer); println()
+        }
+        printer.print(")")
     }
     //deepClone
     override fun deepClone(): TestLinker2Model   {
         return TestLinker2Model(
+            _myString.deepClonePolymorphic(),
+            _myBool.deepClonePolymorphic(),
+            _myEnum.deepClonePolymorphic(),
+            _data.deepClonePolymorphic(),
+            _myStructure.deepClonePolymorphic()
         )
     }
     //contexts
 }
 val Solution.testLinker2Model get() = getOrCreateExtension("testLinker2Model", ::TestLinker2Model)
 
+
+
+/**
+ * #### Generated from [TestLinker2Model.kt:10]
+ */
+enum class MyEnum {
+    FirstValue, 
+    SecondValue;
+    
+    companion object {
+        val marshaller = FrameworkMarshallers.enum<MyEnum>()
+        
+    }
+}
+
+
+/**
+ * #### Generated from [TestLinker2Model.kt:15]
+ */
+data class MyStructure (
+    val projectFile: String,
+    val target: String
+) : IPrintable {
+    //companion
+    
+    companion object : IMarshaller<MyStructure> {
+        override val _type: KClass<MyStructure> = MyStructure::class
+        
+        @Suppress("UNCHECKED_CAST")
+        override fun read(ctx: SerializationCtx, buffer: AbstractBuffer): MyStructure  {
+            val projectFile = buffer.readString()
+            val target = buffer.readString()
+            return MyStructure(projectFile, target)
+        }
+        
+        override fun write(ctx: SerializationCtx, buffer: AbstractBuffer, value: MyStructure)  {
+            buffer.writeString(value.projectFile)
+            buffer.writeString(value.target)
+        }
+        
+        
+    }
+    //fields
+    //methods
+    //initializer
+    //secondary constructor
+    //equals trait
+    override fun equals(other: Any?): Boolean  {
+        if (this === other) return true
+        if (other == null || other::class != this::class) return false
+        
+        other as MyStructure
+        
+        if (projectFile != other.projectFile) return false
+        if (target != other.target) return false
+        
+        return true
+    }
+    //hash code trait
+    override fun hashCode(): Int  {
+        var __r = 0
+        __r = __r*31 + projectFile.hashCode()
+        __r = __r*31 + target.hashCode()
+        return __r
+    }
+    //pretty print
+    override fun print(printer: PrettyPrinter)  {
+        printer.println("MyStructure (")
+        printer.indent {
+            print("projectFile = "); projectFile.print(printer); println()
+            print("target = "); target.print(printer); println()
+        }
+        printer.print(")")
+    }
+    //deepClone
+    //contexts
+}
