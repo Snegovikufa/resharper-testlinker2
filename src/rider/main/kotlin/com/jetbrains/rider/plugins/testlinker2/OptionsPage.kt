3baab2dd825bbe8424399c7d45075e268f954598
Rustam Safin <rustiksnegovik@gmail.com>
2021-11-01 15:06:26 +0200

    Fix settings
    Signed-off-by: Rustam Safin <rustiksnegovik@gmail.com>

--- src/rider/main/kotlin/com/jetbrains/rider/plugins/testlinker2/OptionsPage.kt
+++ src/rider/main/kotlin/com/jetbrains/rider/plugins/testlinker2/OptionsPage.kt
@@ -2,7 +2,7 @@ package com.jetbrains.rider.plugins.testlinker2
 
 import com.jetbrains.rider.settings.simple.SimpleOptionsPage
 
-class OptionsPage : SimpleOptionsPage("Test Linker 2 Options", "OptionsPage") {
+class TestLinker2OptionsPage : SimpleOptionsPage("Test Linker 2 Options", "TestLinker2OptionsPage") {
     override fun getId(): String {
         return "Test Linker 2 Options"
     }
